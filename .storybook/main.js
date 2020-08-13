2e0263c7bc12bce6dd1c2a8e802ce19ca42c5015
liukun <919590347@qq.com>
2020-08-13 23:09:00 +0800

    feat(ts): 添加typescript支持

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,4 +7,13 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
 }
