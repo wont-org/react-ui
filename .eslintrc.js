4c5402c652e681b118c32283863b6134847f08ce
liukun <919590347@qq.com>
2020-08-18 00:22:02 +0800

    feat(prettier): 新增prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,40 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    node: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2020: true,
+        node: true,
     },
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-    'import/no-extraneous-dependencies': 0,
-    'import/no-unresolved': 0,
-    'import/prefer-default-export': 0,
-    'import/extensions': 0,
-    'react/no-unescaped-entities': 0,
-    'react/jsx-filename-extension': [1, {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    }],
-    'react/jsx-props-no-spreading': 0,
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
     ],
-  },
-};
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 11,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+    ],
+    rules: {
+        indent: ['error', 4],
+        semi: ['error', 'never'],
+        'import/no-extraneous-dependencies': 0,
+        'import/no-unresolved': 0,
+        'import/prefer-default-export': 0,
+        'import/extensions': 0,
+        'react/no-unescaped-entities': 0,
+        'react/jsx-filename-extension': [1, {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        }],
+        'react/jsx-props-no-spreading': 0,
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+            'error',
+        ],
+    },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    singleQuote: true,
+    semi: false,
+    tabWidth: 4,
+    endOfLine: 'lf', // lf-mac crlf-win
+}
