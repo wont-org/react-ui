acc0f8d5dcec5cc61c4e17dee27573ad891b7fef
liukun <919590347@qq.com>
2020-08-20 22:17:48 +0800

    feat(webpack: init): 初始化webpack打包

---
+++ build/webpack.config.ts
@@ -0,0 +1,46 @@
+const { join } = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    entry: {
+        button: join(__dirname, '../components/button/Button.tsx'),
+    },
+    output: {
+        path: join(__dirname, '../dist'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [{
+                    loader: 'ts-loader',
+                }],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader',
+                ],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            // filename: '[name]_[contenthash:8].css',
+        }),
+        new OptimizeCssAssetsWebpackPlugin({
+            assetNameRegExp: /\.css$/g,
+        }),
+    ],
+}
