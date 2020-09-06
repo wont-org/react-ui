13dc5fa8e1389bb2c6487b46fbfcea6effc9df00
liukun <919590347@qq.com>
2020-09-06 22:55:05 +0800

    ci: travis 增加npm 自动发版

---
+++ components/index.tsx
@@ -0,0 +1,11 @@
+// 此文件是脚本自动生成，请勿在此修改
+
+import { Button } from './button/button'
+import { Header } from './header/header'
+import { Page } from './page/page'
+
+export default {
+    Button,
+    Header,
+    Page,
+}
