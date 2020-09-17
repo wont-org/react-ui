bbd718ac3c0a7eaa1d6d1cb49bade5d80b86bad8
liukun <919590347@qq.com>
2020-09-18 00:44:36 +0800

    chore: 增加发版链路

---
+++ scripts/pub.ts
@@ -0,0 +1,20 @@
+import {
+    execSync, spawn,
+} from 'child_process'
+import { version } from '../package.json'
+
+const runner = spawn('npm', ['publish'])
+
+runner.on('close', (code) => {
+    if (code === 0) {
+        execSync('git add CHANGELOG.md')
+        execSync(`git commit -m docs: CHANGELOG => ${version}`)
+        execSync('git push')
+        execSync(`git tag v${version}`)
+        execSync(`git push origin master v${version}:v${version}`)
+        // execSync('git push origin master:master')
+    } else {
+        // eslint-disable-next-line no-console
+        console.log(`发布失败，子进程退出，退出码 ${code}`)
+    }
+})
