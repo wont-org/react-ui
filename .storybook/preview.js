ff7f45e0ba6de4da6c9d35a3c7381d7c0f4aa5db
liukun <919590347@qq.com>
2020-09-23 22:48:34 +0800

    build: 完善 actions and controls

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,22 @@
+// import { MINIMAL_VIEWPORTS} from '@storybook/addon-viewport'
+
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: { expanded: true },
+    // viewport: {
+    //     viewports: MINIMAL_VIEWPORTS,
+    // },
+    // backgrounds: {
+    //     default: 'twitter',
+    //     values: [
+    //         {
+    //             name: 'twitter',
+    //             value: '#00aced'
+    //         },
+    //         {
+    //             name: 'facebook',
+    //             value: '#3b5998'
+    //         },
+    //     ],
+    // }
 }
--- components/button/index.stories.tsx
+++ components/button/index.stories.tsx
@@ -4,17 +4,45 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import Button, { ButtonProps } from '.'
 
 export default {
-    title: 'From/Button',
+    title: 'Form/Button',
     component: Button,
     argTypes: {
         // backgroundColor: { control: 'color' },
+        onClick: { action: 'clicked' },
+        label: {
+            type: {
+                required: true,
+            },
+            description: '按钮文案',
+            control: {
+                type: 'text',
+            },
+        },
         type: {
+            description: '按钮类型',
+            table: {
+                defaultValue: {
+                    summary: 'primary',
+                },
+                type: {
+                    summary: ['primary', 'success', 'danger', 'warning', 'info'],
+                },
+            },
             control: {
                 type: 'select',
                 options: ['primary', 'success', 'danger', 'warning', 'info'],
             },
         },
         size: {
+            description: '按钮大小',
+            table: {
+                defaultValue: {
+                    summary: 'medium',
+                },
+                type: {
+                    summary: ['large', 'medium', 'small', 'mini'],
+                },
+            },
             control: {
                 type: 'select',
                 options: ['large', 'medium', 'small', 'mini'],
