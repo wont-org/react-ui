81e8e673e80b73c682f3f1694c47b0e4e15f4815
liukun <919590347@qq.com>
2021-02-13 13:28:17 +0800

    chore: 处理单元测试问题, 更改react-dom依赖

--- tests/shared/mountTest.tsx
+++ tests/shared/mountTest.tsx
@@ -5,9 +5,9 @@ import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
 // eslint-disable-next-line jest/no-export
-export default function mountTest(Component) {
+export default function mountTest(Component: React.ComponentType) {
     describe('mount and unmount', () => {
-    // https://github.com/ant-design/ant-design/pull/18441
+        // https://github.com/ant-design/ant-design/pull/18441
         it('component could be updated and unmounted without errors', () => {
             const wrapper = mount(<Component />)
             expect(() => {
